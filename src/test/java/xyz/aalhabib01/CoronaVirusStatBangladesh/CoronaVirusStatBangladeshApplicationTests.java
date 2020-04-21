0788f20fdf5cf74e620ee2ed65e7bfee0c8853fa
Habib <aalhabib001@gmail.com>
2020-04-21 14:58:11 +0600

    Added Data

---
+++ src/test/java/xyz/aalhabib01/CoronaVirusStatBangladesh/CoronaVirusStatBangladeshApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.aalhabib01.CoronaVirusStatBangladesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusStatBangladeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
